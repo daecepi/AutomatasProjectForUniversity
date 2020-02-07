59878202cafcebe88cf911f42b4159946da317cb
David Cermeño <davidcermeno@MacBook-Pro-de-David.local>
2020-02-07 12:39:36 -0500

    Updates to code conversion

--- src/Generator/GeneradorDeC.java
+++ src/Generator/GeneradorDeC.java
@@ -8,6 +8,8 @@ package Generator;
 import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.List;
+
+import jdk.swing.interop.SwingInterOpUtils;
 import logica.analizadorLexico.IDefaultToken;
 import java.util.ArrayList;
 
@@ -36,7 +38,7 @@ public class GeneradorDeC extends Generador{
 
          }*/
          
-         String codeBuilt = "#include <stdio.h>; \n\n // function declaration";
+         String codeBuilt = "#include <stdio.h> \n\n // function declaration";
          String mainFunctionCode = "";
          String functionDeclaration = "";
          String functionsDefinitions = "";
@@ -67,26 +69,34 @@ public class GeneradorDeC extends Generador{
         String returned = "";
         for (int i = 0; i < tokensCopy.size(); i++){
             IDefaultToken token = null;
-            try{
-                token = tokensCopy.remove(i);
-            }catch (Exception e){
-                return null ;
-            }
+
+            //try{
+            token = tokensCopy.get(i);
+            //}catch (Exception e){
+            //    return null ;
+            //}
+
+            System.out.println((token.getKey()));
             switch (token.getKey()){
                 case "Apertura":
-                    returned.concat(convertIf(token, tokensCopy));
+                    System.out.println("entre");
+                    returned += (convertIf(token, tokensCopy));
                     break;
-                case "Asignement":
-                    returned.concat(convertAssignement(token, tokensCopy));
+                case "Asignación":
+                    System.out.println("entre");
+                    returned += (convertAssignement(token, tokensCopy));
                     break;
                 case "Escrebir":
-                    returned.concat(convertWrite(token, tokens));
+                    System.out.println("entre");
+                    returned += (convertWrite(token, tokensCopy));
                     break;
                 case "Leer":
-                    returned += convertRead(token, tokens);
+                    System.out.println("entre");
+                    returned += convertRead(token, tokensCopy);
                     break;
                 case "Vacío":
-                    returned += token.getValue();
+                    System.out.println("entre");
+                    returned += token.getValue().replace("#", "//");
                     break;
                 case "Cierre":
                     returned += "}";
@@ -95,6 +105,8 @@ public class GeneradorDeC extends Generador{
             returned += "\n";
 
         }
+
+        System.out.println("Hello"+returned);
 
          return returned;
     }
@@ -116,10 +128,10 @@ public class GeneradorDeC extends Generador{
                 if(value.indexOf("\"") != -1){ //Variable is a String
                     int count = value.replace("*", "").length();
                     System.out.println("Still the same string must be " + value);
-                    finalSentence += "char* " + variableName + "[" + count + "]";
+                    finalSentence += "char " + variableName + "[" + count + "]";
 
                     dataType = "%s";
-                }else if (value.indexOf(".") != -1){ // Data type is float
+                }else if (value.contains(".") || value.contains(",")){ // Data type is float
                     finalSentence += "double " + variableName;
 
                     dataType = "%f";
@@ -134,7 +146,11 @@ public class GeneradorDeC extends Generador{
             }else{
                 finalSentence += variableName;
             }
-            finalSentence += " = " + value + ";";
+            if(value.contains("$")){
+                finalSentence += " = " + value.replace("$", "") + ";";
+            }else{
+                finalSentence += " = " + value + ";";
+            }
         }catch (Exception e){
             return null;
         }
@@ -143,36 +159,41 @@ public class GeneradorDeC extends Generador{
 
     @Override
     public String convertWrite(IDefaultToken token, List<IDefaultToken> tokens){
-         String finalSentence = "printf(";
+         String finalSentence = "printf(\"";
+
+         System.out.println("Entre write");
 
          // Quitamos las palabras reservadas
          String toPrint = token.getValue().replace("ESCRIBIR", "").replace("MOSTRAR", "").replace("IMPRIMIR", "").replace(";", "");
 
-         finalSentence.concat("\""); // Start of string
+        System.out.println("Entre write");
 
          // Atendemos a la concatenacion
-        String[] listOfPrints = toPrint.split("+");
+        String[] listOfPrints = toPrint.split("+", 2);
         String listForTypes = "";
+
 
         for(String print : listOfPrints){
             if (print.contains("\"") || print.contains("'")){ // string
-                finalSentence.concat("%s");
+                finalSentence += ("%s");
 
-                listForTypes.concat(", " + print);
+                listForTypes += (", " + print);
             }else if(print.contains("$")){ // variable
                 String variableName = print.replace("$", "");
-                finalSentence.concat(this.listOfVariables.get(variableName).toString());
+                finalSentence += (this.listOfVariables.get(variableName).toString());
 
-                listForTypes.concat(", " + variableName);
+                listForTypes += (", " + variableName);
             }else{ // number
-                finalSentence.concat("%d");
+                finalSentence += ("%d");
 
-                listForTypes.concat(", " + print);
+                listForTypes += (", " + print);
             }
         }
 
-        finalSentence.concat("\", "); // End of string
-        finalSentence.concat(listForTypes.toString());
+        System.out.println("Entre write");
+
+        finalSentence +=("\", "); // End of string
+        finalSentence += (listForTypes);
 
         finalSentence += ");";
 
@@ -187,14 +208,13 @@ public class GeneradorDeC extends Generador{
     public String convertRead(IDefaultToken token, List<IDefaultToken> tokens){
          String finalSentence = "";
 
-
-
          return finalSentence;
     }
 
     @Override
     public String convertIf(IDefaultToken token, List<IDefaultToken> tokens) {
-        return null;
+        String finalSentence = token.getValue().replace("SINO", "}else").replace(":=", "=").replace("SI", "if").replace(":","{").replace("$", "").replace("MIENTRAS", "while").replace("PARA", "for");
+        return finalSentence;
     }
 
     @Override
